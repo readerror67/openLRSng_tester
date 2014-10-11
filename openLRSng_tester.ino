db13dfee48776ae037ca387d91a5bf26effaa883
readerror67 <readerror67@users.noreply.github.com>
2014-10-11 02:38:46 -0400

    Initial commit just to test RF switch/diversity. Removed some stuff, added some stuff.

--- openLRSng_tester.ino
+++ openLRSng_tester.ino
@@ -9,202 +9,7 @@
 // 4 = OpenLRSngTX (tbd.)
 #define HW 4
 
-#if (HW==0)
-#define PPM_IN           A5
-#define RF_OUT_INDICATOR A4
-#define BUZZER           9
-#define BTN              10
-#define PPM_Pin_Interrupt_Setup  PCMSK1 = 0x20;PCICR|=(1<<PCIE1);
-#define PPM_Signal_Interrupt PCINT1_vect
-#define PPM_Signal_Edge_Check ((PINC & 0x20)==0x20)
-
-void buzzerInit()
-{
-  pinMode(BUZZER_ACT, OUTPUT);
-  digitalWrite(BUZZER_ACT, LOW);
-}
-
-void buzzerOn(uint16_t freq)
-{
-  if (freq) {
-    digitalWrite(BUZZER_ACT,HIGH);
-  } else {
-    digitalWrite(BUZZER_ACT,LOW);
-  }
-}
-
-#define buzzerOff(foo) buzzerOn(0)
-#define Red_LED    12
-#define Green_LED  11
-
-#define Red_LED_ON  PORTB |= _BV(4);
-#define Red_LED_OFF  PORTB &= ~_BV(4);
-
-#define Green_LED_ON   PORTB |= _BV(3);
-#define Green_LED_OFF  PORTB &= ~_BV(3);
-
-//## RFM22B Pinouts for Public Edition (M1 or Rx v1)
-#define  nIRQ_1 (PIND & 0x08)==0x08 //D3
-#define  nIRQ_0 (PIND & 0x08)==0x00 //D3
-
-#define  nSEL_on PORTD |= (1<<4) //D4                                                                                                                                                   #define  nSEL_off PORTD &= 0xEF //D4
-
-#define  SCK_on PORTD |= (1<<2) //D2                                                                                                                                                    #define  SCK_off PORTD &= 0xFB //D2
-
-#define  SDI_on PORTC |= (1<<1) //C1                                                                                                                                                    #define  SDI_off PORTC &= 0xFD //C1
-
-#define  SDO_1 (PINC & 0x01) == 0x01 //C0                                                                                                                                               #define  SDO_0 (PINC & 0x01) == 0x00 //C0
-
-#define SDO_pin A0
-#define SDI_pin A1
-#define SCLK_pin 2
-#define IRQ_pin 3
-#define nSel_pin 4
-
-#define IRQ_interrupt 1
-
-#elif (HW==2)
-#define PPM_IN 3
-#define RF_OUT_INDICATOR A0
-#define BUZZER 10
-#define BTN 11
-#define Red_LED 13
-#define Green_LED 12
-
-#define Red_LED_ON  PORTB |= _BV(5);
-#define Red_LED_OFF  PORTB &= ~_BV(5);
-
-#define Green_LED_ON   PORTB |= _BV(4);
-#define Green_LED_OFF  PORTB &= ~_BV(4);
-
-#define PPM_Pin_Interrupt_Setup  PCMSK2 = 0x08;PCICR|=(1<<PCIE2);
-#define PPM_Signal_Interrupt PCINT2_vect
-#define PPM_Signal_Edge_Check (PIND & 0x08)==0x08
-
-void buzzerInit()
-{
-  pinMode(BUZZER, OUTPUT);
-  digitalWrite(BUZZER, LOW);
-}
-
-void buzzerOn(uint16_t freq)
-{
-  if (freq) {
-    digitalWrite(BUZZER,HIGH);
-  } else {
-    digitalWrite(BUZZER,LOW);
-  }
-}
-
-#define buzzerOff(foo) buzzerOn(0)
-
-//## RFM22B Pinouts for Public Edition (M2)
-#define  nIRQ_1 (PIND & 0x04)==0x04 //D2
-#define  nIRQ_0 (PIND & 0x04)==0x00 //D2
-
-#define  nSEL_on PORTD |= (1<<4) //D4
-#define  nSEL_off PORTD &= 0xEF //D4
-
-#define  SCK_on PORTD |= (1<<7) //D7
-#define  SCK_off PORTD &= 0x7F //D7
-
-#define  SDI_on PORTB |= (1<<0) //B0
-#define  SDI_off PORTB &= 0xFE //B0
-
-#define  SDO_1 (PINB & 0x02) == 0x02 //B1
-#define  SDO_0 (PINB & 0x02) == 0x00 //B1
-
-#define SDO_pin 9
-#define SDI_pin 8
-#define SCLK_pin 7
-#define IRQ_pin 2
-#define nSel_pin 4
-
-#define IRQ_interrupt 0
-
-#elif (HW==3)
-
-#define USE_ICP1 // use ICP1 for PPM input for less jitter
-
-#ifdef USE_ICP1
-#define PPM_IN 8 // ICP1
-#else
-#define PPM_IN 3
-#define PPM_Pin_Interrupt_Setup  PCMSK2 = 0x08;PCICR|=(1<<PCIE2);
-#define PPM_Signal_Interrupt PCINT2_vect
-#define PPM_Signal_Edge_Check (PIND & 0x08)==0x08
-#endif
-
-#define BUZZER 6
-#define BUZZER2 3
-#define BTN 7
-
-void buzzerInit()
-{
-  pinMode(BUZZER, OUTPUT);
-  digitalWrite(BUZZER, LOW);
-  TCCR2A = (1<<WGM21); // mode=CTC
-  TCCR2B = (1<<CS22) | (1<<CS20); // prescaler = 128
-  pinMode(BUZZER2, OUTPUT);
-  digitalWrite(BUZZER2, LOW);
-}
-
-void buzzerOn(uint16_t freq)
-{
-  if (freq) {
-    digitalWrite(BUZZER,HIGH);
-    uint32_t ocr = 125000L / freq;
-    if (ocr>255) {
-      ocr=255;
-    }
-    if (!ocr) {
-      ocr=1;
-    }
-    OCR2A = ocr;
-    TCCR2A |= (1<<COM2B0); // enable output on buzzer2
-  } else {
-    digitalWrite(BUZZER,LOW);
-    TCCR2A &= ~(1<<COM2B0); // disable output buzzer2
-  }
-}
-
-#define buzzerOff(foo) buzzerOn(0)
-
-#define Red_LED A3
-#define Green_LED 13
-#define Red_LED2   9
-#define Green_LED2 10
-
-#define Red_LED_ON  { PORTC |= _BV(3); PORTB |= _BV(1); }
-#define Red_LED_OFF { PORTC &= ~_BV(3); PORTB &= ~_BV(1); }
-#define Green_LED_ON  PORTB |= (_BV(5) | _BV(2));
-#define Green_LED_OFF PORTB &= ~(_BV(5) | _BV(2));
-
-//## RFM22B Pinouts for Public Edition (Rx v2)
-#define  nIRQ_1 (PIND & 0x04)==0x04 //D2
-#define  nIRQ_0 (PIND & 0x04)==0x00 //D2
-
-#define  nSEL_on PORTD |= (1<<4) //D4
-#define  nSEL_off PORTD &= 0xEF //D4
-
-#define  SCK_on PORTC |= (1<<2) //A2
-#define  SCK_off PORTC &= 0xFB //A2
-
-#define  SDI_on PORTC |= (1<<1) //A1
-#define  SDI_off PORTC &= 0xFD //A1
-
-#define  SDO_1 (PINC & 0x01) == 0x01 //A0
-#define  SDO_0 (PINC & 0x01) == 0x00 //A0
-
-#define SDO_pin A0
-#define SDI_pin A1
-#define SCLK_pin A2
-#define IRQ_pin 2
-#define nSel_pin 4
-
-#define IRQ_interrupt 0
-
-#elif (HW==4)
+#if (HW==4)
 
 #define USE_ICP1 // use ICP1 for PPM input for less jitter
 #define PPM_IN 8 // ICP1
@@ -708,6 +513,16 @@ uint8_t rfmGetRSSI(void)
   return spiReadRegister(0x26);
 }
 
+uint8_t rfmGetRSSI1(void)
+{
+  return spiReadRegister(0x28);
+}
+
+uint8_t rfmGetRSSI2(void)
+{
+  return spiReadRegister(0x29);
+}
+
 void rssiTest(void)
 {
   Serial.println("RSSI test");
@@ -719,9 +534,32 @@ void rssiTest(void)
   to_rx_mode();
 
   spiWriteRegister(0x1c, 0x12);   // 41.7kHz
+
+  // Enable diversity stuff..
+  spiWriteRegister(0x0d, 0xf7); // GPIO2 configuration
+  spiWriteRegister(0x08, 0xe0); // enables ant div algorithm (111)
+  
+  uint8_t ant1 = 0;
+  uint8_t ant2 = 0;
+  uint8_t ant1_max = 0;
+  uint8_t ant2_max = 0;
+
   rfmSetCarrierFrequency(435000000);
   while (!Serial.available()) {
-    Serial.println(rfmGetRSSI());
+    ant1 = rfmGetRSSI1();
+    ant2 = rfmGetRSSI2();
+    if (ant1 > ant1_max) ant1_max = ant1;
+    if (ant2 > ant2_max) ant2_max = ant2;
+    
+    Serial.print("[");
+    Serial.print(ant1_max);
+    Serial.print("]  ");
+    Serial.print(ant1);
+
+    Serial.print(" -- [");
+    Serial.print(ant2_max);
+    Serial.print("]  ");
+    Serial.println(ant2);
     delay(100);
   }
   spiWriteRegister(0x07, RF22B_PWRSTATE_READY);
